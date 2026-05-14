ae7f8e0fd9e8a1bd10e84feba142a4d4ae29b6f9
Sanya Singh <sanya20903@gmail.com>
2026-05-14 22:22:10 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -4,10 +4,9 @@ public class Main
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int num, rev=0, d=0, temp=0;
+        int num, rev=0, d=0;
         System.out.println("Enter number: ");
         num = in.nextInt();
-        temp = num;
         
         while(num!=0)
         {
