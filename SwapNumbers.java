bd75b3918754d2db556e6c6359d88af12cb7b4d2
Sanya Singh <sanya20903@gmail.com>
2026-05-14 21:22:14 +0530

    Create SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, temp=0;
+        System.out.println("Enter 1st number: ");
+        num1 = in.nextInt();
+        System.out.println("Enter 2nd number: ");
+        num2 = in.nextInt();
+        
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        
+        System.out.println("Numbers after swapping: ");
+        System.out.println("num1 = " +num1);
+        System.out.println("num2 = " +num2);
+    }
+}
