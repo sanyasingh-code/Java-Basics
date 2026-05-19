eb2748bb0984f68a254aa634156306600ac70283
Sanya Singh <sanya20903@gmail.com>
2026-05-20 00:32:28 +0530

    Create AverageOfArray.java

---
+++ AverageOfArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int arr[]= new int[5];
+        System.out.println("Enter elements: ");
+        for(int i=0; i<5; i++)
+        {
+            arr[i]= in.nextInt();
+        }
+        for(int i=0; i<5; i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        int sum=0;
+        float avg=0;
+        for(int i=0; i<5; i++)
+        {
+            sum = sum + arr[i];
+        }
+        avg = (float)sum/5;
+        System.out.println("Average of elements in the array: " +avg);
+    }
+}
