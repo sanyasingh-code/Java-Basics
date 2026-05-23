a18c9b52ea0bbb79b40c3a86e20861d72d84fa63
Sanya Singh <sanya20903@gmail.com>
2026-05-23 23:38:44 +0530

    Create ArrayDeletingElementAtSpecificPosition.java

---
+++ ArrayDeletingElementAtSpecificPosition.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 2, 3, 4, 5};
+        int pos=2;
+        
+        for(int i = pos; i < arr.length-1; i++)
+            {
+                arr[i] = arr[i+1];
+            }
+    
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
