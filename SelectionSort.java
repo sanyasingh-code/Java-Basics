0ffcfb18ca37c72a7ec622f5bdafb0c6064aa690
Sanya Singh <sanya20903@gmail.com>
2026-05-23 18:47:59 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {5, 3, 1, 4, 2};
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[j] < arr[min])
+                {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.println("Sorted Array:");
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
