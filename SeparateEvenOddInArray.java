4827d26efc236e44829fc176b248bd3d941b99dc
Sanya Singh <sanya20903@gmail.com>
2026-05-23 22:35:44 +0530

    Create SeparateEvenOddInArray.java

---
+++ SeparateEvenOddInArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        System.out.print("Even elements: ");
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] % 2 == 0)
+            {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.print("Odd elements: ");
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] % 2 != 0)
+            {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
