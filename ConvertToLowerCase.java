b7fd78a692c0f74485d5d649990e5b8c7e339cec
Sanya Singh <sanya20903@gmail.com>
2026-05-25 16:24:53 +0530

    Create ConvertToLowerCase.java

---
+++ ConvertToLowerCase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String str = "HELLO";
+        String str1 = str.toLowerCase();
+        System.out.println("Upper case to Lower case: " + str1);
+    }
+}
