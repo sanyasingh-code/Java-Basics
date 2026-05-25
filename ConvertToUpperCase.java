99f816ce4b27a5e6e038655ed593215f16a6d2b2
Sanya Singh <sanya20903@gmail.com>
2026-05-25 16:26:59 +0530

    Create ConvertToUpperCase.java

---
+++ ConvertToUpperCase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String str = "hello";
+        String str1 = str.toUpperCase();
+        System.out.println("Lower case to Upper case: " + str1);
+    }
+}
