025da82395d29d9ce2b5dab924c137d1aa1af02d
Sanya Singh <sanya20903@gmail.com>
2026-05-25 15:54:57 +0530

    Create CountVowel.java

---
+++ CountVowel.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String str = in.nextLine();
+        String str1 = str.toUpperCase();
+        int len = str1.length();
+        int vowel=0;
+        
+        for(int i=0; i<len; i++)
+        {
+            char ch = str1.charAt(i);
+            if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+            {
+                vowel++;
+            }
+        }
+        System.out.print("Number of Vowels: " + vowel);
+    }
+}
+        
